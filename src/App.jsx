7c6338595d2807efd40065ca8633205b591f40a4
Ira <ira.kumpula@metropolia.fi>
2025-11-13 11:54:20 +0200

    Add start for week4 tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import './App.css';
+import Greeting from './components/Greeting'; // ei tarvitse olla .jsx-päätettä
+import Footer from './components/Footer';
 
 // JS-funktio, joka palauttaa JSX
 const App = () => {
+  const sitename = 'Week 4 React';
+
+  // style pitää olla camelCasella
+  const styles = {
+    backgroundColor: 'gray',
+    color: 'white',
+  }
+
   return (
+    // tyhjä div - kaikki pitää olla yhden elementin (fragmentin) sisällä
+
+    // <Greeting /> ja <Footer /> tuotu komponenteista, huom. notaatio. Voi käyttää useasti samaa komponenttia
     <>
-      <h1>Week 4 React App</h1>
-      <p>Some text</p>
+      <h1 style={styles}>{sitename} Sivusto</h1>
+      <div style={{color: 'red'}}>Moikka moi</div>
+      <Greeting name="Ulla" age={39} isTeacher={true}/>
+      <Greeting name="Matti" age={42} isTeacher={false}/>
+      <ul>
+        <li>Moi</li>
+        <li>Haloo</li>
+        <li>Piisamirotta</li>
+      </ul>
+
+      <Footer />
+      <Footer />
+      <Footer />
     </>
   );
 };
