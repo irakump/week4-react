55dac1515168e607fad66c1bc0cd07d109179bcd
Ira <ira.kumpula@metropolia.fi>
2025-11-13 10:48:20 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+// JS-funktio, joka palauttaa JSX
+const App = () => {
+  return (
+    <>
+      <h1>Week 4 React App</h1>
+      <p>Some text</p>
+    </>
+  );
+};
+
+export default App;
