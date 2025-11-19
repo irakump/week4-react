336f5efc67f3f73a2f00abd5e1a2fbfa12d93945
Ira <ira.kumpula@metropolia.fi>
2025-11-19 22:48:35 +0200

    Change base to / instead of my-sub-path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: 'my-sub-path/',
+  //base: '/',
 });
 
 // base pitää olla '/~omakäyttäjä/my-sub-path/' jos julkaistu Metropolia shellissä; julkaise juureen
+// tai '/~omakäyttäjä/' riippuen mikä on base
