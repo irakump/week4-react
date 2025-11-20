64e25a08f2e4b4f99373e7f76ef58157be4dd700
Ira <ira.kumpula@metropolia.fi>
2025-11-20 09:08:19 +0200

    Set Metropolia shell username to base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'my-sub-path/',
+  base: '/~iraku/routing/',
 });
-
-// base pitää olla '/~omakäyttäjä/my-sub-path/' jos julkaistu Metropolia shellissä; julkaise juureen
