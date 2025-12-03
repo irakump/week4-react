990f3a5e7ecc725524628be86a573feb79da22e9
Ira <ira.kumpula@metropolia.fi>
2025-12-03 15:34:06 +0200

    Add tailwind to Single component

--- src/views/Single.jsx
+++ src/views/Single.jsx
@@ -8,11 +8,11 @@ const imageOrVideo = (item) => {
   }
 
   if (item.media_type == 'image/jpeg') {
-    return <img src={item.thumbnail} alt={item.title} />;
+    return <img src={item.thumbnail} alt={item.title} className="max-w-full rounded-md m-5 align-" />;
   } else if (item.media_type == 'video/mp4') {
-    return <video src={item.filename} controls></video>
+    return <video src={item.filename} controls className="max-w-full rounded-md m-5" ></video>
   } else {
-    return <div>Media type not supported</div>
+    return <div className="max-w-full rounded-md text-lg m-5 text-red-500" >Media type not supported</div>
   }
 };
 
@@ -22,18 +22,18 @@ const Single = () => {
   const navigate = useNavigate();
 
   return (
-    <>
-    <h2>Single item</h2>
+    <div className="flex flex-col">
+    <h2 className="font-bold text-xl">Single item</h2>
       {item && (
-        <dialog open>
-          <div>{item.title}</div>
-          <div>{item.description}</div>
+        <dialog open className=" items-center fixed inset-30 p-4 justify-center">
+          <div className="m-5 font-bold">{item.title}</div>
+          <div className="m-5">{item.description}</div>
           {imageOrVideo(item)}
 
-          <button onClick={() => navigate(-1)}>Go back</button>
+          <button onClick={() => navigate(-1)} className="m-5 p-3 bg-blue-400 hover:bg-blue-500 text-lg rounded">Go back</button>
         </dialog>
       )}
-    </>
+    </div>
   );
 };
 
